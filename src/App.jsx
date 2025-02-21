4c99194fe5c48e87bcc3f592b468dffb04737973
TruffleBug <lisa.ho1022@gmail.com>
2025-02-21 16:08:11 -0700

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,11 +7,39 @@ import Gamecards from './Gamecards'
 export default function App() {
   const [shuffledData, setShuffledData] = useState([]);
   const [clickedCards, setClickedCards] = useState([]);
-  const params = {shuffledData, setShuffledData, clickedCards, setClickedCards};
   const [highScore, setHighScore] = useState(0);
+  // const [data, setData] = useState([]);
+  const params = {shuffledData, setShuffledData, clickedCards, setClickedCards};
+  const [generatedPokemons, setGeneratedPokemons] = useState([]);
 
   const currScore = clickedCards.length;
   if(currScore > highScore) setHighScore(currScore);
+
+  // generate array of 12 numbers
+  let randomNums = [];
+  for(let i = 0; i < 12; i++) {
+    let randomNum = Math.floor(Math.random() * (151) + 1);
+    if(!randomNums.includes(randomNum)) {
+      randomNums.push(randomNum);
+    };
+  };
+
+  console.log('randomNums', randomNums)
+
+  useEffect(() => {
+    const pokemonArray = [];
+    randomNums.forEach(async (num) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`) 
+      const json = await response.json();
+      pokemonArray.push({
+        id: num,
+        name: json.forms[0].name,
+        imgURL: json.sprites.front_default
+      })
+    })
+    setGeneratedPokemons(pokemonArray)
+  console.log('generatedPokemons', generatedPokemons)
+  }, [])
 
   return (
     <>
@@ -19,8 +47,9 @@ export default function App() {
       <h2>Click on each image just once to win the game!</h2>
       <section className='counter'>Score: {currScore}</section>
       <section className='highScore'>High Score: {highScore}</section>
+      {/* <section className='highScore'>genpoke: {generatedPokemons}</section> */}
       <section className='gameboard'>
-        <Gamecards params={params}/>
+        {/* <Gamecards params={params}/> */}
       </section>
     </>
   )
--- src/Gamecards.jsx
+++ src/Gamecards.jsx
@@ -8,59 +8,52 @@ const cardIds = [
 ];
 
 export default function Gamecards({ params }) {
-    if (!params) console.log('test')
     const shuffledData = params.shuffledData;
     const setShuffledData = params.setShuffledData;
     const clickedCards = params.clickedCards;
     const setClickedCards = params.setClickedCards;
-    const [data, setData] = useState([]);
-    
+    const data = params.data;
 
-    // shuffles cardIds array
+    // shuffles data array
     useEffect(() => {
         const shuffleArray = (array) => {
             const newArray = [...array];
             for(let i = newArray.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-            }
+            };
             return newArray;
         };
-        setShuffledData(shuffleArray(cardIds));
-    }, [clickedCards, setShuffledData])
+        setShuffledData(shuffleArray(data));
+    }, [setShuffledData, data]);
+    
+    // ----------------------------------------------------------
+    console.log('shuffledData', shuffledData)
 
-    useEffect(() => {
-        fetch('https://dog.ceo/api/breeds/image/random/12')
-        .then(response => response.json())
-        .then(json => setData(json))
-        .catch(error => console.error(error));
+    const card = shuffledData.map(c => {
+        return (
+            <section className="gamecard" key={c.name} onClick={() => handleClick(c.name)}>
+                <img src={c.url.sprites[front_default]} alt={c.name} />
+                <h1>{c.name}</h1>
+            </section>
+        ) 
+    });
 
-        console.log('data' , data.message)
-    }, []);
-
-    const card = shuffledData.map(c =>
-        <section className="gamecard" key={c} onClick={() => handleClick(c)}>
-            <section><h1>{c}</h1></section>
-            <section>text</section>
-            <img src={data.message[0]}/>
-        </section>
-    )
-
-    function handleClick(c) {
-        if (!clickedCards.includes(c)) {
-            clickedCards.push(c)
+    function handleClick(name) {
+        if (!clickedCards.includes(name)) {
+            clickedCards.push(name)
             setClickedCards([...clickedCards])
         } else {
             handleGameOver()
         }
         console.log('clickedCards =', clickedCards)
-    }
+    };
 
     function handleGameOver() {
         console.log('game over')
         alert(`Game Over`)
         setClickedCards([]);
-    }
+    };
 
     return (
         <>
