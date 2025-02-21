9e43fed77ea09319ffface83cdc52bc9d4e49fa5
TruffleBug <lisa.ho1022@gmail.com>
2025-02-21 11:46:43 -0700

    Game works w/o images

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import './App.css'
 import Gamecards from './Gamecards'
 
 export default function App() {
+  const [shuffledData, setShuffledData] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const params = {shuffledData, setShuffledData, clickedCards, setClickedCards};
+  const [highScore, setHighScore] = useState(0);
 
+  const currScore = clickedCards.length;
+  if(currScore > highScore) setHighScore(currScore);
 
   return (
     <>
       <h1>Memory Game</h1>
       <h2>Click on each image just once to win the game!</h2>
-      <section className='counter'>Score: </section>
-      <section className='highScore'>High Score: </section>
+      <section className='counter'>Score: {currScore}</section>
+      <section className='highScore'>High Score: {highScore}</section>
       <section className='gameboard'>
-        <Gamecards />
+        <Gamecards params={params}/>
       </section>
     </>
   )
--- src/Gamecards.jsx
+++ src/Gamecards.jsx
@@ -7,30 +7,69 @@ const cardIds = [
     1,2,3,4,5,6,7,8,9,10,11,12
 ];
 
-export default function Gamecards() {
-    const [data, setData] = useState(null);
+export default function Gamecards({ params }) {
+    if (!params) console.log('test')
+    const shuffledData = params.shuffledData;
+    const setShuffledData = params.setShuffledData;
+    const clickedCards = params.clickedCards;
+    const setClickedCards = params.setClickedCards;
+    const [data, setData] = useState([]);
+    
+
+    // shuffles cardIds array
+    useEffect(() => {
+        const shuffleArray = (array) => {
+            const newArray = [...array];
+            for(let i = newArray.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+            }
+            return newArray;
+        };
+        setShuffledData(shuffleArray(cardIds));
+    }, [clickedCards, setShuffledData])
 
     useEffect(() => {
-        fetch('https:api/fruit/all')
+        fetch('https://dog.ceo/api/breeds/image/random/12')
         .then(response => response.json())
         .then(json => setData(json))
         .catch(error => console.error(error));
+
+        console.log('data' , data.message)
     }, []);
 
+    const card = shuffledData.map(c =>
+        <section className="gamecard" key={c} onClick={() => handleClick(c)}>
+            <section><h1>{c}</h1></section>
+            <section>text</section>
+            <img src={data.message[0]}/>
+        </section>
+    )
 
+    function handleClick(c) {
+        if (!clickedCards.includes(c)) {
+            clickedCards.push(c)
+            setClickedCards([...clickedCards])
+        } else {
+            handleGameOver()
+        }
+        console.log('clickedCards =', clickedCards)
+    }
 
-
-    // const card = cardIds.map(c =>
-    //     <section className="gamecard" key={c}>
-    //         <section>{c}</section>
-    //         <section>text</section>
-    //     </section>
-    // )
+    function handleGameOver() {
+        console.log('game over')
+        alert(`Game Over`)
+        setClickedCards([]);
+    }
 
     return (
-        // <>{card}</>
-        <div>
-            {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-        </div>
+        <>
+            {card}
+        </>
+
+
+        // <div>
+        //     {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        // </div>
     )
 }
